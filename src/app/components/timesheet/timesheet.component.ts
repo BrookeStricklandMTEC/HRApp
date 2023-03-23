65f29cab4c98b6c3207cf181f1c3b0bc5df0dfbe
Brooke Strickland <brooke.strickland2904@stu.mtec.edu>
2023-03-23 10:13:00 -0600

    nearly finsihed

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -17,6 +17,7 @@ export class TimesheetComponent implements OnInit {
     department: Department | undefined;
     employeeNameFC = new FormControl('', this.nameValidator());    employees: Employee[] = [];
     employeeId = 0;
+    weekdays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']; 
 
   constructor(
     private route: ActivatedRoute,
@@ -37,11 +38,18 @@ export class TimesheetComponent implements OnInit {
             departmentId: this.department?.id,
             name: this.employeeNameFC.value,
             payRate: Math.floor(Math.random() * 50) + 50,
+            monday: 0,
+            tuesday: 0,
+            wednesday: 0,
+            thursday: 0,
+            friday: 0,
+            saturday: 0,
+            sunday: 0
         });
 
         this.employeeNameFC.setValue('');
     }
-  }
+}
 
   nameValidator(): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
@@ -56,4 +64,14 @@ export class TimesheetComponent implements OnInit {
         return error;
     };
 }
+
+getTotalHours(employee: Employee): number {
+  return employee.monday + employee.tuesday + employee.wednesday
+      + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+}
+
+deleteEmployee(index: number): void {
+  this.employees.splice(index, 1);
+}
+
 }
